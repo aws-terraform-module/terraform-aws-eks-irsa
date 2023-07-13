f9343286e16d95c971a5256ea0470424455e5447
Mr Nim <mr.nim94@gmail.com>
2023-07-13 17:13:16 +0000

    initial

---
+++ c2-01-generic-variables.tf
@@ -0,0 +1,73 @@
+# Input Variables - Placeholder file
+# AWS Region
+variable "aws_region" {
+  description = "Region in which AWS Resources to be created"
+  type = string
+  default = "us-west-2"  
+}
+# Environment Variable
+variable "environment" {
+  description = "Environment Variable used as a prefix"
+  type = string
+  default = "staging"
+}
+# Business Division
+variable "business_divsion" {
+  description = "Business Division in the large organization this Infrastructure belongs"
+  type = string
+  default = "argo-workflow-nim"
+}
+
+# Business Division
+variable "eks_cluster_id" {
+  description = "The ID of the EKS cluster. Note: currently a value is returned only for local EKS clusters created on Outposts"
+  type = string
+  default = "staging-nim-engines"
+}
+
+variable "aws_iam_openid_connect_provider_arn" {
+  description = "The ARN assigned by AWS for this provider/data.terraform_remote_state.eks.outputs.aws_iam_openid_connect_provider_arn"
+  type = string
+  default = ""
+}
+
+variable "eks_cluster_endpoint" {
+  description = "The hostname (in form of URI) of Kubernetes master/data.terraform_remote_state.eks.outputs.cluster_endpoint"
+  type = string
+  default = ""
+}
+
+variable "eks_cluster_certificate_authority_data" {
+  description = "PEM-encoded root certificates bundle for TLS authentication./data.terraform_remote_state.eks.outputs.cluster_certificate_authority_data"
+  type = string
+  default = ""
+}
+
+variable "json_policy" {
+  description = "Input Policy that is used for IRSA"
+  type = string
+  default = <<EOT
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Effect": "Allow",
+      "Action": [
+        "s3:ListBucket",
+        "s3:GetBucketLocation"
+      ],
+      "Resource": "*"
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "s3:GetObject",
+        "s3:PutObject",
+        "s3:DeleteObject"
+      ], 
+      "Resource": "*"
+    }
+  ]
+}
+EOT
+}
---
+++ c3-01-irsa-iam-policy-and-role.tf
@@ -0,0 +1,59 @@
+# output "aws_iam_openid_connect_provider_arn" {
+#   description = "aws_iam_openid_connect_provider_arn"
+#   value = "arn:aws:iam::${element(split(":", "${data.aws_eks_cluster.k8s.arn}"), 4)}:oidc-provider/${element(split("//", "${data.aws_eks_cluster.k8s.identity[0].oidc[0].issuer}"), 1)}"
+# }
+
+# Resource: IAM Policy for Cluster Autoscaler
+resource "aws_iam_policy" "irsa_iam_policy" {
+  name        = "${local.name}-IRSAPolicy"
+  path        = "/"
+  description = "IRSA IAM Policy"
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  policy = var.json_policy
+}
+
+#data.terraform_remote_state.eks.outputs.aws_iam_openid_connect_provider_arn
+#data.terraform_remote_state.eks.outputs.aws_iam_openid_connect_provider_extract_from_arn
+
+# Resource: Create IAM Role and associate the EBS IAM Policy to it
+resource "aws_iam_role" "irsa_iam_role" {
+  name = "${local.name}-irsa-iam-role"
+
+  # Terraform's "jsonencode" function converts a Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        Action = "sts:AssumeRoleWithWebIdentity"
+        Effect = "Allow"
+        Sid    = ""
+        Principal = {
+          Federated = "${var.aws_iam_openid_connect_provider_arn}"
+        }
+        Condition = {
+          StringLike = {            
+            "${local.aws_iam_openid_connect_provider_extract_from_arn}:sub": "system:serviceaccount:argo-workflow:${local.name}-argo-workflows-*"
+          }
+        }        
+
+      },
+    ]
+  })
+
+  tags = {
+    tag-key = "${local.name}-irsa-iam-role"
+  }
+}
+
+# Associate IAM Role and Policy
+resource "aws_iam_role_policy_attachment" "irsa_iam_role_policy_attach" {
+  policy_arn = aws_iam_policy.irsa_iam_policy.arn 
+  role       = aws_iam_role.irsa_iam_role.name
+}
+
+output "irsa_iam_role_arn" {
+  description = "IRSA Demo IAM Role ARN"
+  value = aws_iam_role.irsa_iam_role.arn
+}
